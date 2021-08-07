0b11b9ea5f00abbd7304a394c100071eba0cad89
soulrrrrr <chialun100@gmail.com>
2021-08-07 23:51:04 +0800

    fixed comments and bug

--- binary_search.cpp
+++ binary_search.cpp
@@ -6,19 +6,25 @@ using namespace std;
 #define pii pair<int, int>
 #define fs first
 #define sc second
+#define SoulRRRRR AC
 
+/*----------------------------------------------------------------
+
+Problem: https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/A
+
+------------------------------------------------------------------*/
 const int N = 1e6+10;
 int w, h, n;
 const int mod = 998244353;
 vector<int> v(100005);
-//https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/A
+
 void io() {
     ios_base::sync_with_stdio(0);
 	cin.tie(0);
 }
 
+//avoid overflow
 int findr() {
-    //避免overflow
     int r = 1;
     while((r/w)*(r/h) < n)
         r <<= 1;
@@ -29,15 +35,15 @@ signed main() {
     io();
     cin >> w >> h >> n;
     int l = 0, r = findr();
-    while(l < r) {
+    while(l + 1 < r) {
         int m = (l + r) / 2;
-        if ((m/w)*(m/h) >= n)
+        if ((m/w)*(m/h) >= n) // can achieve
             r = m;
-        else l = m;
+        else l = m; // can't achieve
     }
-    // l:0的最右邊
-    // r:1的最左邊
-    cout << l << endl;
+    // l: maximum of can't achieve
+    // r: minimum of can achieve
+    cout << r << endl;
 
     return 0;
 }
